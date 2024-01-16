382d4ff37d02fefe51f04621849dec4a53d30aa4
sam <samuel.j.peter@gmail.com>
2024-01-16 13:30:35 +0100

    logging, reload, fix member stats

--- src/webling/app.py
+++ src/webling/app.py
@@ -2,27 +2,42 @@ import altair as alt
 import pandas as pd
 import streamlit as st
 from streamlit.logger import get_logger
-from member import Member
+from members import Member, MemberGroup
 from rest_calls import get_call
-from datetime import date
+from datetime import date, datetime
 
 
 LOGGER = get_logger(__name__)
+STATE_KEY = 'load_counter'
+
+def get_members() -> list[Member]:
+    # data = get_call('membergroup', {'format': 'full'}).json()
+    # groups = [MemberGroup(**group) for group in data]
+    # LOGGER.info('Loaded %d groups from Webling' % len(groups))
+    data = get_call('member', {'format': 'full'}).json()
+    members = [Member(**member) for member in data]
+    LOGGER.info('Loaded %d members from Webling' % len(members))
+    return members
+
 
 def run():
+    LOGGER.info('Starting app')
+
     st.set_page_config(
         page_title='LC ZH DS',
         page_icon=':palm_tree:',
     )
+    st.session_state[STATE_KEY] = 0
 
     st.title("LC Zürich Doppelstock Mitgliederstatistik")
     st.write("Diese App visualisiert die Mitgliederstatistik von LC Zürich Doppelstock.")
     st.write("Die Daten werden anonymisiert von Webling geladen und in der App verarbeitet.")
     st.write("Die Daten werden nicht gespeichert oder weiterverarbeitet.")
 
-    data = get_call('member', {'format': 'full'}).json()
-    members = [Member(**member) for member in data]
-    
+    LOGGER.info('Loading members')
+    members = get_members()
+
+    LOGGER.info('Creating chart')
     years = range(2016, date.today().year + 1)
     m_count = [len([member for member in members if member.is_active(year) and not member.talent]) for year in years]
     t_count = [len([member for member in members if member.is_active(year) and member.talent]) for year in years]
@@ -42,6 +57,10 @@ def run():
 
     st.altair_chart(chart, use_container_width=True)
 
+    if st.button("re-load data"):
+        members = get_members()
+        st.info('Loaded %d members from Webling (%s)' % (len(members), datetime.now().strftime('%H:%M:%S')))
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     run()
--- src/webling/members.py
+++ src/webling/members.py
@@ -2,10 +2,18 @@ from datetime import date
 from pydantic import AliasPath, BaseModel, Field
 
 
+class MemberGroup(BaseModel):
+    name: str = Field(validation_alias=AliasPath('properties', 'title'))
+    members: list[int] = Field(validation_alias=AliasPath('children', 'member'))
+    
+
 class Member(BaseModel):
     talent: bool = Field(validation_alias=AliasPath('properties', 'Talent'))
+    status: str = Field(validation_alias=AliasPath('properties', 'Status'))
     start: date = Field(validation_alias=AliasPath('properties', 'Eintrittsdatum'))
     end: date | None = Field(default=None, validation_alias=AliasPath('properties', 'Austrittsdatum'))
 
     def is_active(self, year: int) -> bool:
+        if self.status == 'Aspirant':
+            return False
         return self.start.year <= year and (self.end is None or year < self.end.year)
