eb34b89cf28560f3320cae4c46bbe1aafe1511a2
sam <samuel.j.peter@gmail.com>
2024-01-25 08:50:36 +0100

    remove debugging flag, new plots

--- src/pages/1_Race_Analysis.py
+++ src/pages/1_Race_Analysis.py
@@ -1,12 +1,10 @@
 import pandas as pd
 import streamlit as st
-import plotly.express as px
 from utils import page_config
 from data import get_google_sheet
-from plot_config import CONFIGS
+from race_plots import RACE_PLOTS
 from streamlit.logger import get_logger
 
-DEBUGGING = False
 
 LOGGER = get_logger(__name__)
 
@@ -26,8 +24,6 @@ df_raw = get_google_sheet('1yKpap4SXwDw6r8-JPViEgNwJw_a-POd13Y47790tGwo', races[
 df_raw = df_raw.set_index('skier')
 df_raw = df_raw.apply(pd.to_datetime, format='%H:%M:%S')
 
-if DEBUGGING: df_raw = df_raw.head(4)
-
 # skiers selection
 selected_skiers = st.sidebar.multiselect('Select skiers', df_raw.index)
 if selected_skiers == []:
@@ -36,29 +32,11 @@ if selected_skiers == []:
 filtered_df = df_raw.loc[selected_skiers]
 
 # select a plot type and pre-process the data
-plot_type = st.sidebar.selectbox('Select a plot type', CONFIGS)
-if plot_type is None:
+plot = st.sidebar.selectbox('Select a plot type', RACE_PLOTS)
+if plot is None:
     st.stop()
-df = plot_type.pre_process(filtered_df)
-
-if DEBUGGING: st.dataframe(df)
-
-# Convert the dataframe to long format
-df_long = df.reset_index().melt(id_vars='skier', var_name='km', value_name='time')
-# Convert the km column to numeric
-df_long['km'] = pd.to_numeric(df_long['km'])
-
-# Create a line plot for the selected skiers using Plotly
-fig = px.line(
-    df_long, x='km', y='time', color='skier',
-    title=plot_type.name,
-    labels={
-        'km': 'Race Distance [km]',
-        'time': plot_type.y_label
-    })
 
 # Display the line plot for the selected skiers
-st.write(plot_type.explanation)
+fig = plot.make_figure(filtered_df)
+st.write(plot.explanation)
 st.plotly_chart(fig, use_container_width=True)
-
-if DEBUGGING: st.dataframe(df_long)
--- src/race_plots.py
+++ src/race_plots.py
@@ -1,9 +1,12 @@
 from typing import Callable
 import pandas as pd
+import plotly.express as px
+from plotly.graph_objs import Figure
 from pydantic_settings import BaseSettings
 
 
-class PlotConfig(BaseSettings):
+class RacePlot(BaseSettings):
+
     name: str
     explanation: str
     y_label: str
@@ -11,9 +14,24 @@ class PlotConfig(BaseSettings):
 
     def __str__(self) -> str:
         return self.name
+    
+    def make_figure(self, data: pd.DataFrame) -> Figure:
+        df = self.pre_process(data)
+        # Convert the dataframe to long format
+        df_long = df.reset_index().melt(id_vars='skier', var_name='km', value_name='time')
+        # Convert the km column to numeric
+        df_long['km'] = pd.to_numeric(df_long['km'])
+        fig = px.line(
+            df_long, x='km', y='time', color='skier',
+            title=self.name,
+            labels={
+                'km': 'Race Distance [km]',
+                'time': self.y_label
+            })
+        return fig
 
 
-def make_diff_to_fastest(data: pd.DataFrame) -> pd.DataFrame:
+def make_diff_to_winner(data: pd.DataFrame) -> pd.DataFrame:
     km = [int(i) for i in data.columns]
     # find column with end time
     end_time = str(max(km))
@@ -26,18 +44,19 @@ def make_diff_to_fastest(data: pd.DataFrame) -> pd.DataFrame:
     return df
 
 
-def make_diff_to_fastest_section(data: pd.DataFrame) -> pd.DataFrame:
-    df = make_diff_to_fastest(data)
+def make_diff_to_winner_section(data: pd.DataFrame) -> pd.DataFrame:
+    df = make_diff_to_winner(data)
     df = df.diff(axis=1)
+    km = [int(i) for i in data.columns]
+    df = df * 60 / km
+    df.fillna(0, inplace=True)
     return df
 
     
 def make_diff_to_leader(data: pd.DataFrame) -> pd.DataFrame:
     km = [int(i) for i in data.columns]
-    # find column with end time
+    # find fastest time per section
     min_times = data.min()
-    # find fastest skier
-    #fastest_skier = data[end_time].idxmin()
     # calculate difference to fastest skier, assuming to have started at the same time
     df = data.sub(min_times)
     df = df.apply(pd.to_numeric)/(60*1e9)
@@ -45,24 +64,24 @@ def make_diff_to_leader(data: pd.DataFrame) -> pd.DataFrame:
     return df
 
 
-CONFIGS = [
-    PlotConfig(
+RACE_PLOTS = [
+    RacePlot(
         name='vs. leader of selection',
         explanation='Time gap to the leader of the race at the specific checkpoint, considering only the selected skiers.',
         y_label='Time difference [min]',
         pre_process=make_diff_to_leader),
-    PlotConfig(
+    RacePlot(
         name='vs. winner of selection',
         explanation='Time difference to the eventual winner of the race, considering only the selected skiers.',
         y_label='Time difference [min]',
-        pre_process=make_diff_to_fastest),
-    PlotConfig(
+        pre_process=make_diff_to_winner),
+    RacePlot(
         name='Race Time',
         explanation='',
         y_label='Time [HH:MM:SS]'),
-    PlotConfig(
-        name='vs. fastest from selection, diff per section',
-        explanation='???',
-        y_label='Time difference [min]',
-        pre_process=make_diff_to_fastest_section)
+    RacePlot(
+        name='sec/km vs. winner from selection, ',
+        explanation='Speed difference between checkpoints against eventual winner, considering only the selected skiers.',
+        y_label='Speed difference [sec/km]',
+        pre_process=make_diff_to_winner_section)
 ]
