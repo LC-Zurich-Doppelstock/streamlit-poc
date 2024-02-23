8dca7ba4d5b30eea567da3830b95024b59424c5b
Samuel Peter <samuel.j.peter@gmail.com>
2024-02-23 17:21:18 +0100

    waether for races (#10)
    * waether for races
    
    * api key for weather

--- src/data.py
+++ src/data.py
@@ -1,10 +1,12 @@
 import pandas as pd
+import streamlit as st
 from pydantic_settings import BaseSettings
 from requests import get, Response
 from typing import Any
+from urllib import parse
 
 
-class ApiConfig(BaseSettings):
+class WeblingConfig(BaseSettings):
     apiUrl: str = 'https://zuerichdoppelstock.webling.ch/api/1'
     apiKey: str = ''
 
@@ -13,7 +15,7 @@ class ApiConfig(BaseSettings):
         env_prefix = 'WEBLING_'
 
 
-cfg = ApiConfig()
+cfg = WeblingConfig()
 
 
 def get_call(route: str, params: dict[str, Any] = {}) -> Response:
@@ -24,3 +26,21 @@ def get_call(route: str, params: dict[str, Any] = {}) -> Response:
 def get_google_sheet(spreadsheet_id: str, sheet_id: str, format: str = 'csv') -> pd.DataFrame:
     url = f'https://docs.google.com/spreadsheets/d/{spreadsheet_id}/export?format={format}&gid={sheet_id}'
     return pd.read_csv(url)
+
+
+class VisualCrossingConfig(BaseSettings):
+    apiUrl: str = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services'
+    apiKey: str = ''
+
+    class Config:
+        env_file = '.env'
+        env_prefix = 'VISUALCROSSING_'
+
+
+vc_cfg = VisualCrossingConfig()
+
+@st.cache_data
+def load_forecast(location: str, id: int) -> pd.DataFrame:
+    loc = parse.quote_plus(location)
+    url = f'{vc_cfg.apiUrl}/timeline/{loc}?unitGroup=metric&include=hours&key={vc_cfg.apiKey}&contentType=csv'
+    return pd.read_csv(url, delimiter=',', index_col='datetime', parse_dates=True)
