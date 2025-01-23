7b8c67045b6341c7318f1287c94adbdb77c9e426
Samuel Peter <samuel.j.peter@gmail.com>
2025-01-23 23:06:45 +0100

    Fix/multi login (#20)
    * fix session state
    
    * LoginHandler class stored as sessions state
    
    * cleanup

--- src/data.py
+++ src/data.py
@@ -1,11 +1,14 @@
 import pandas as pd
 import streamlit as st
+import os
 from pydantic_settings import BaseSettings
 from requests import get, Response
 from typing import Any
 from urllib import parse
 from webling.members import Member, Status
-from supabase import create_client, Client
+from supabase import create_client, Client, AuthApiError, AuthWeakPasswordError
+from gotrue.types import User, SignUpWithEmailAndPasswordCredentials
+from typing import Callable
 
 
 class WeblingConfig(BaseSettings):
@@ -69,17 +72,69 @@ class SupaBaseConfig(BaseSettings):
 
 supabase_cfg = SupaBaseConfig()
 
-@st.cache_resource
-def get_supabase_client() -> Client:
-    if not supabase_cfg.url or not supabase_cfg.key:
-        st.error('SUPABASE_URL or SUPABASE_KEY not set')
-        st.stop()
-    client = create_client(supabase_cfg.url, supabase_cfg.key)
-    try:
-        client.auth.sign_in_with_password({
-            "email": supabase_cfg.email,
-            "password": supabase_cfg.password
-        })
-    except Exception as e:
-        pass
-    return client
+class LoginHandler:
+    supabase_client: Client
+    authorized_user: User | None
+
+    def __init__(self):
+        if not supabase_cfg.url or not supabase_cfg.key:
+            st.error('SUPABASE_URL or SUPABASE_KEY not set')
+            st.stop()
+        self.supabase_client = create_client(supabase_cfg.url, supabase_cfg.key)
+        try:
+            self.supabase_client.auth.sign_in_with_password({
+                "email": supabase_cfg.email,
+                "password": supabase_cfg.password
+            })
+        except Exception:
+            pass
+
+    def set_authorized_user(self):
+        user_response = self.supabase_client.auth.get_user()
+        self.authorized_user = user_response.user if user_response else None
+
+    def get_authorized_user(self) -> User | None:
+        return self.authorized_user
+
+    def is_logged_in(self) -> bool:
+        return self.authorized_user is not None
+    
+    def login(self, email: str, password: str):
+        def login_func():
+            member = get_active_member(email)
+            response = self.supabase_client.auth.sign_in_with_password({
+                "email": email,
+                "password": password
+            })
+            st.success("Logged in successfully.")
+            self.set_authorized_user()
+            st.rerun()
+        self._handle_action(login_func)
+    
+    def signup(self, email: str, password: str):
+        def signup_func():
+            member = get_active_member(email)
+            app_url = os.getenv("APP_URL")
+            if not app_url:
+                st.error("APP_URL not set")
+                st.stop()
+            body = SignUpWithEmailAndPasswordCredentials(
+                email=email,
+                password=password,
+                options={
+                    "email_redirect_to": app_url + "/Supabase"
+                }
+            )
+            response = self.supabase_client.auth.sign_up(body)
+            if response.user and response.user.identities and len(response.user.identities) > 0:
+                st.success("User created successfully. Please check your email for a verification link.")
+            else:
+                st.warning("User already exists. Please log in.")
+        self._handle_action(signup_func)
+    
+    def _handle_action(self, func: Callable[[], None]):
+        try:
+            func()
+        except (AuthWeakPasswordError, AuthApiError) as e:
+            st.error(e)
+            st.stop()
--- src/pages/4_Supabase.py
+++ src/pages/4_Supabase.py
@@ -1,85 +1,40 @@
-import os
 import streamlit as st
-from data import get_active_member, get_supabase_client
-from supabase import AuthApiError, AuthWeakPasswordError
+from data import get_active_member, LoginHandler
 from models.kick_wax import KickWaxEntry, KickWaxAdd
-from typing import Callable
 from pydantic import ValidationError
 import streamlit_react_jsonschema as srj
 
 
-supabase = get_supabase_client()
-
-LOGIN_STATE = "logged_in"
-
-def set_login_state(state: bool):
-    st.session_state[LOGIN_STATE] = state
-
-def is_logged_in() -> bool:
-    return st.session_state.get(LOGIN_STATE, False)    
-
-def login(email: str, password: str):
-    member = get_active_member(email)
-    response = supabase.auth.sign_in_with_password({
-        "email": email,
-        "password": password
-    })
-    st.success("Logged in successfully.")
-    set_login_state(True)
-    st.rerun()
-
-def signup(email: str, password: str):
-    member = get_active_member(email)
-    app_url = os.getenv("APP_URL")
-    if not app_url:
-        st.error("APP_URL not set")
-        st.stop()
-    body = {
-        "email": email,
-        "password": password,
-        "options": {
-            "email_redirect_to": app_url + "/Supabase"
-        }
-    }
-    response = supabase.auth.sign_up(body)
-    if len(response.user.identities) > 0:
-        st.success("User created successfully. Please check your email for a verification link.")
-    else:
-        st.warning("User already exists. Please log in.")
-
-def handle_action(clicked: bool, func: Callable[[], None]):
-    if clicked:
-        try:
-            func()
-        except (AuthWeakPasswordError, AuthApiError) as e:
-            st.error(e)
-            st.stop()
+if "login_handler" not in st.session_state:
+    st.session_state["login_handler"] = LoginHandler()
+login_handler = st.session_state["login_handler"]
 
 def init():
     # initial login state
-    set_login_state(supabase.auth.get_user() is not None)
+    login_handler.set_authorized_user()
     # if not logged in, show login form
-    if not is_logged_in():
+    if not login_handler.is_logged_in():
         with st.sidebar.expander("You are not logged in. Please log in or register."):
             with st.form("login_form"):
                 email = st.text_input("Email", help="Your e-mail address listed in Webling")
                 password = st.text_input("Password", type="password")
                 col1, col2 = st.columns(2)
                 with col1:
-                    login_button = st.form_submit_button("Log In", type="primary")
+                    if st.form_submit_button("Log In", type="primary"):
+                        login_handler.login(email, password)
                 with col2:
-                    signup_button = st.form_submit_button("Register", type="secondary", help="Register a new user")
+                    if st.form_submit_button("Register", type="secondary", help="Register a new user"):
+                        login_handler.signup(email, password)
                 # TODO: Add a button to reset password
-                handle_action(login_button, lambda: login(email, password))
-                handle_action(signup_button, lambda: signup(email, password))
+                st.stop()
     # otherwise show the user info
     else:
         with st.sidebar.expander("You are logged in."):
-            user = supabase.auth.get_user()
+            user = login_handler.get_authorized_user()
             if not user:
                 st.error("No User logged in.")
                 st.stop()
-            member = get_active_member(user.user.email)
+            member = get_active_member(user.email)
             if not member:
                 st.error("No active member found in Webling with email {user.user.email}.")
                 st.stop()
@@ -87,8 +42,7 @@ def init():
 
 # Main
 init()
-if not is_logged_in():
-    st.stop()
+supabase = login_handler.supabase_client
 
 # load data
 data = supabase.table("kickwax").select("*").execute().data
@@ -103,7 +57,14 @@ summary = [
     } for entry in data
 ]
 display_columns.append('mine')
-table = st.dataframe(summary, use_container_width=True, hide_index=True, column_order=display_columns, on_select="rerun", selection_mode="single-row")
+# display table
+table = st.dataframe(
+    summary,
+    use_container_width=True,
+    hide_index=True,
+    column_order=display_columns,
+    on_select="rerun",
+    selection_mode="single-row")
 
 # view/edit/delete
 if not table.selection.rows:
