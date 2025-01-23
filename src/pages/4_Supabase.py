39d68ee58264b1f0b40d2fa82f0caff59075d6da
sam <samuel.j.peter@gmail.com>
2025-01-23 12:54:07 +0100

    simple delete journey

--- src/pages/4_Supabase.py
+++ src/pages/4_Supabase.py
@@ -2,7 +2,7 @@ import os
 import streamlit as st
 from data import get_active_member, get_supabase_client
 from supabase import AuthApiError, AuthWeakPasswordError
-from models.kick_wax import KickWaxEntry
+from models.kick_wax import KickWaxEntry, KickWaxAdd
 from typing import Callable
 from pydantic import ValidationError
 import streamlit_react_jsonschema as srj
@@ -94,29 +94,45 @@ if not is_logged_in():
 data = supabase.table("kickwax").select("*").execute().data
 
 # Select only relevant columns for display
+user_id = supabase.auth.get_user().user.id
 display_columns = ['date', 'name', 'location', 'success_rate']
-summary = [{k: v for k, v in entry.items() if k in display_columns} for entry in data]
-
+summary = [
+    {
+        **{k: v for k, v in entry.items() if k in display_columns},
+        **{'mine': entry.get('created_by') == user_id}
+    } for entry in data
+]
+display_columns.append('mine')
 table = st.dataframe(summary, use_container_width=True, hide_index=True, column_order=display_columns, on_select="rerun", selection_mode="single-row")
 
-# view details
+# view/edit/delete
 if not table.selection.rows:
-    st.info("Select a row from the table above to view the layers.")
+    st.info("Select a row from the table.")
 else:
     selected_index = table.selection.rows[0]
     entry = KickWaxEntry(**data[selected_index])
+        
+    # view
     st.subheader(f"Wax layers for {entry.name} on {entry.date}")
     for layer in entry.layers:
         st.write(str(layer))
+    
+    # delete
+    with st.expander(f"Delete {entry}"):
+        if st.button("Yes, delete", key="confirm_delete", type="primary"):
+            supabase.table("kickwax").delete().eq("id", entry.id).execute()
+            st.success("Deleted entry.")
+            st.rerun()
+        if st.button("No, cancel", key="cancel_delete", type="secondary"):
+            st.rerun()
 
 # add new entry
 with st.expander("Add new entry"):
-    value, submitted = srj.pydantic_form(model=KickWaxEntry)    
+    value, submitted = srj.pydantic_form(model=KickWaxAdd)   
     if submitted and value:
         try:
-            obj = KickWaxEntry.model_validate(value)
-            st.write(obj.model_dump())
+            obj = KickWaxAdd.model_validate(value)
             supabase.table("kickwax").insert(obj.model_dump()).execute()
-            st.success("Added new entry.")
+            st.success(f"Added new entry {obj}")
         except ValidationError as e:
             st.error(e)
